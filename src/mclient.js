e1f5532666301e760029a5a15296a6950bfd0904
ben wei <ben@staros.mobi>
2012-08-24 12:46:16 +0800

    support /who command to list all inline users

--- src/mclient.js
+++ src/mclient.js
@@ -4,10 +4,79 @@ var WebSocket = require('ws');
 
 var stdin = process.openStdin();
 
-var messages = [];
-
 var sender = 'guest';
 var host = 'localhost';
+
+function ChatClient(wsurl) {
+    var self = this;
+    var ws = new WebSocket(wsurl);
+    console.log("connecting to " + wsurl);
+
+    var banner= { cmd: "register", data: "mclient v0.1", sender: sender };
+
+    ws.on('open', function() {
+        ws.send(JSON.stringify(banner));
+
+    });
+
+    this.cmdfunc = function (cmd, args) {
+        try {
+            var blob = JSON.stringify({cmd: cmd, data: ''});
+//            console.log(blob);
+            ws.send(blob);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    this.send = function (data) {
+        try {
+            if (data[0] == 47) {
+                var str = data.toString();
+                var i = str.indexOf(' ');
+                var cmd;
+                var args= '';
+                if (i > 1) {
+                    cmd = str.substring(1, i-1);
+                    args = str.slice(i+1);
+                } else if (str.length > 3) {
+                    cmd = str.substring(1, str.length);
+                }
+                if (cmd)
+                    self.cmdfunc(cmd, args);
+            } else {
+                ws.send(data)
+            }
+        } catch (e) {
+            console.log("send error:" + e);
+        }
+    }
+
+    ws.on('close', function(code, data) {
+        ws.terminate();
+        console.log("disconnected." + code + ":" + data);
+        process.exit(0);
+    });
+
+    ws.on('message', function(data, flags) {
+        var obj = null;
+        var sender = null;
+        var message = null;
+        // flags.binary will be set if a binary data is received
+        // flags.masked will be set if the data was masked
+        try {
+            obj = JSON.parse(data);
+            sender = obj['sender'];
+            message = obj['data'];
+        } catch (e) { };
+
+        if (sender) {
+            console.log(sender + "> " + message);
+        } else {
+            console.log("> " + data);
+        }
+    });
+};
 
 if (process.argv.length > 2)
     sender = process.argv[2];
@@ -16,50 +85,12 @@ if (process.argv.length > 3)
     host = process.argv[3];
 
 var url = 'ws://' + host + ':8009';
-console.log("connect to " + url);
-var ws = new WebSocket(url);
-
-var banner= { cmd: "register", data: "mclient v0.1", sender: sender };
-
-ws.on('open', function() {
-    ws.send(JSON.stringify(banner));
-
-    setInterval(function () {
-        if (messages.length > 0) {
-            for (var i = 0; i < messages.length ; i++)
-                ws.send(messages[i])
-            messages = [];
-        }
-    }, 100);
-});
-
-ws.on('close', function() {
-    console.log("disconnected.");
-});
-
-ws.on('message', function(data, flags) {
-    var obj = null;
-    var sender = null;
-    var message = null;
-    // flags.binary will be set if a binary data is received
-    // flags.masked will be set if the data was masked
-    try {
-        obj = JSON.parse(data);
-        sender = obj['sender'];
-        message = obj['data'];
-    } catch (e) { };
-
-    if (sender) {
-        console.log(sender + "> " + message);
-    } else {
-        console.log("> " + data);
-    }
-});
+var wsclient = new ChatClient(url);
 
 stdin.on('data', function(chunk) {
     //console.log("Got chunk: " + chunk); }
     var data = chunk.slice(0, chunk.length - 1);
     if (data.length > 0) {
-        messages.push(data);
+        wsclient.send(data);
     }
 });
--- src/mserver.js
+++ src/mserver.js
@@ -37,79 +37,137 @@ function findCurrentSender(ws) {
     return null;
 }
 
-wss.on('connection', function(ws) {
-    num++;
-    clients.push({ws: ws, id:num});
+var WSC_PERM_ADMIN = 0,
+WSC_PERM_USER = 1,
+WSC_PERM_GUEST = 2;
 
-    ws.on('message', function(data) {
-        var mws = ws;
-        var obj = null;
-        var cmd = null;
-        var message = null;
-        var sender = null;
+var WSC_STAT_CONNECTED = 1,
+WSC_STAT_TERMINATE = 2;
+
+
+function cmd_register(user, obj, message) {
+    try {
+        user.sender= obj['sender'];
+    } catch (e) {};
+
+    if (user.sender)
+        console.log('r%d(%s): %s', user.id, user.sender, message);
+    else
+        console.log('r%d: %s', user.id, message);
+    return 0;
+}
+
+function cmd_who (user, obj, message) {
+    for (var i = 0;  i < clients.length; i++) {
+        var c = clients[i];
+        var msg = '';
+            msg += (c == user) ? '* ' : '  ';
+
+        msg += c.id + ":";
+
+        if (c.hasOwnProperty('sender')) {
+            msg += c.sender;
+        } else {
+            msg += 'guest';
+        }
+
         try {
-            obj = JSON.parse(data);
-            if (obj.hasOwnProperty('data')){
-                message = obj['data'];
-            }
-            if (obj.hasOwnProperty('sender')){
-                sender = obj['sender'];
-            }
-            if (obj.hasOwnProperty('cmd')){
-                cmd = obj['cmd'];
-            }
-            console.log(data);
-        } catch (e) {};
+            user.ws.send(msg);
+        } catch (e) {
+            console.log("error: "  + e);
+        }
+    }
+    return 0;
+}
 
-        if (!message)
-            message = data;
+var user_commander = {
+    who: cmd_who,
+    register: cmd_register };
 
-        var cur = findCurrentSender(ws);
-        if (cur) {
-                if (cmd == 'register') {
-                    try {
-                        cur.sender= obj['sender'];
-                    } catch (e) {};
-                    if (cur.sender)
-                        console.log('r%d(%s): %s', cur.id, cur.sender, message);
-                    else
-                        console.log('r%d: %s', cur.id, message);
+
+    wss.on('connection', function(ws) {
+        num++;
+        clients.push({ws: ws, id:num,
+            msgcount: 0,
+            perm: WSC_PERM_GUEST,
+            state: WSC_STAT_CONNECTED
+        });
+
+        ws.on('message', function(data) {
+            var mws = ws;
+            var obj = null;
+            var cmd = null;
+            var message = null;
+            var sender = null;
+            try {
+                obj = JSON.parse(data);
+                if (obj.hasOwnProperty('data')){
+                    message = obj['data'];
                 }
-        }
+                if (obj.hasOwnProperty('sender')){
+                    sender = obj['sender'];
+                }
+                if (obj.hasOwnProperty('cmd')){
+                    cmd = obj['cmd'];
+                }
+                console.log(data);
+            } catch (e) {};
 
+            if (!message)
+                message = data;
 
-        for (var i=0; i < clients.length ; i++) {
-            var c = clients[i];
-            var blob;
-            if (cur.hasOwnProperty('sender')) {
-                blob = {sender: cur.sender, data: message};
-            } else if (sender) {
-                blob = {sender: sender, data: message};
-            } else {
-                blob = {sender: 'guest', data: message};
+            var user = findCurrentSender(ws);
+            if (user) {
+                var pf = user_commander[cmd];
+                if (pf) {
+                    var ret = pf(user, obj, message);
+                    // no broadcast if ret is zero
+                    if (ret == 0) return;
+                } else if (cmd) {
+                    user.ws.send("unsupported cmd: " + cmd);
+                    return;
+                }
             }
-            c.ws.send(JSON.stringify(blob));
-        }
+
+            if (user.state == WSC_STAT_TERMINATE) {
+                user.ws.close(3000, 'admin stop');
+                return;
+            }
+
+            user.msgcount++;
+            for (var i=0; i < clients.length ; i++) {
+                var c = clients[i];
+                var blob;
+                if (user.hasOwnProperty('sender')) {
+                    blob = {sender: user.sender, data: message};
+                } else if (sender) {
+                    blob = {sender: sender, data: message};
+                } else {
+                    blob = {sender: 'guest', data: message};
+                }
+                c.ws.send(JSON.stringify(blob));
+            }
+
+        });
+
+        ws.on('close', function () {
+            var t = null;
+            var mws = ws;
+            for (var i=0; i < clients.length ; i++) {
+                var c = clients[i];
+                if (c.ws == mws) {
+                    t = c;
+                    clients.remove(t);
+                    break;
+                }
+            }
+            if (t) {
+                console.log('r%d: exit', t.id);
+            }
+        });
+
+        ws.send('Connected to chat server');
+        console.log("conn num: " + clients.length);
     });
 
-    ws.on('close', function () {
-        var t = null;
-        var mws = ws;
-        for (var i=0; i < clients.length ; i++) {
-            var c = clients[i];
-            if (c.ws == mws) {
-                t = c;
-                clients.remove(t);
-                break;
-            }
-        }
-        if (t) {
-            console.log('r%d: exit', t.id);
-        }
-    });
-
-    ws.send('Connected to chat server');
-    console.log("conn num: " + clients.length);
-});
-
-console.log("jschat server listen on " +  server_port + "...");
+    console.log("jschat server listen on " +  server_port + "...");
