cf9e52e129f69d2d605eb7373a8e83137989faa8
Ben Wei <ben@staros.mobi>
2014-05-07 11:19:37 +0800

    [feature] add timestamp for cache messages

--- src/mserver.js
+++ src/mserver.js
@@ -29,12 +29,21 @@ util = require('util'),
 express = require('express'),
 app = express();
 
+
+function UTCTimestamp() {
+    var now = new Date(); 
+    var now_utc = new Date(
+            now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),
+            now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+    return now_utc.getTime();
+}
+
 nconf.file({file: __dirname + '/config.json'});
 var default_port_num = 8009;
 
 var port_num = nconf.get("port");
 if (!port_num) {
-	port_num = default_port_num;
+    port_num = default_port_num;
 }
 
 Array.prototype.remove= function(){
@@ -72,14 +81,14 @@ function findUserByID(id) {
 }
 
 var WSC_PERM_ADMIN = 0,
-WSC_PERM_USER = 1,
-WSC_PERM_GUEST = 2;
+    WSC_PERM_USER = 1,
+    WSC_PERM_GUEST = 2;
 
 var WSC_STAT_CONNECTED = 1,
-WSC_STAT_TERMINATE = 2;
+    WSC_STAT_TERMINATE = 2;
 
 var WSC_CMD_NO_BROADCAST = 0,
-WSC_CMD_BROADCAST = 1;
+    WSC_CMD_BROADCAST = 1;
 
 function cmd_register(user, obj, message) {
     try {
@@ -93,14 +102,13 @@ function cmd_register(user, obj, message) {
     return WSC_CMD_NO_BROADCAST;
 }
 
-function compose_message_blob(sender, message) {
-        return JSON.stringify({sender: sender, data: message});
-}
 
 var cache_messages = [];
 
 function cmd_msg_to(user, obj, message) {
     var target_id = '';
+    var blob = {};
+    blob.sender = user.sender;
     if (obj.hasOwnProperty('target_id')) {
         target_id = obj['target_id'];
     } else {
@@ -111,18 +119,21 @@ function cmd_msg_to(user, obj, message) {
             console.log('target_id:' + target_id);
             console.log('target_msg:' + message);
         } else {
-            var msg = compose_message_blob(user.sender, "syntax: /msg <id> <message string>");
+            blob.message = "syntax: /msg <id> <message string>";
+            var msg = JSON.stringify(blob);
             user.ws.send(msg);
             return WSC_CMD_NO_BROADCAST;
         }
     }
 
-    target = findUserByID(target_id);
+    var target = findUserByID(target_id);
+    blob.data = message;
+    blob.ts = UTCTimestamp();
+
+    var msg = JSON.stringify(blob);
     if (target) {
-        var msg = compose_message_blob(user.sender, message);
         target.ws.send(msg);
     } else {
-        var msg = compose_message_blob(user.sender, "target user not found");
         user.ws.send(msg);
     }
     cache_messages.push(msg);
@@ -133,7 +144,7 @@ function cmd_who (user, obj, message) {
     for (var i = 0;  i < clients.length; i++) {
         var c = clients[i];
         var msg = '';
-            msg += (c == user) ? '* ' : '  ';
+        msg += (c == user) ? '* ' : '  ';
 
         msg += c.id + ":";
 
@@ -167,166 +178,169 @@ var cmd_help = function (user) {
 
 var user_commander = {
 msg: [cmd_msg_to, 'send message to specific user'],
-who: [cmd_who, 'list who online'],
-register: [cmd_register, null], //'register user identity'],
-help: [cmd_help, 'command helper']
+     who: [cmd_who, 'list who online'],
+     register: [cmd_register, null], //'register user identity'],
+     help: [cmd_help, 'command helper']
 };
 
 var server_port = port_num;
 
 if (process.argv.length >=3)
-server_port = process.argv[2];
+    server_port = process.argv[2];
 
-function ChatServer () {
+    function ChatServer () {
 
-    app.use(express.static(__dirname + '/public'));
-    app.use(function (req, res, next) {
-        console.log(req.url);
-        next();
-    });
+        app.use(express.static(__dirname + '/public'));
+        app.use(function (req, res, next) {
+                console.log(req.url);
+                next();
+                });
 
-    var server = http.createServer(app);
-    server.listen(server_port);
+        var server = http.createServer(app);
+        server.listen(server_port);
 
-    var wss = new WebSocketServer({server: server});
+        var wss = new WebSocketServer({server: server});
 
-    wss.on('connection', function(ws) {
-        num++;
+        wss.on('connection', function(ws) {
+                num++;
 
-        clients.push({ws: ws, id:num,
-            msgcount: 0,
-            perm: WSC_PERM_GUEST,
-            state: WSC_STAT_CONNECTED
-        });
+                clients.push({ws: ws, id:num,
+                    msgcount: 0,
+                    perm: WSC_PERM_GUEST,
+                    state: WSC_STAT_CONNECTED
+                    });
 
-        ws.on('message', function(data) {
-            var mws = ws;
-            var obj = null;
-            var cmd = null;
-            var message = null;
-            var sender = null;
-            try {
-                obj = JSON.parse(data);
-                if (obj.hasOwnProperty('data')){
+                ws.on('message', function(data) {
+                    var mws = ws;
+                    var obj = null;
+                    var cmd = null;
+                    var message = null;
+                    var sender = null;
+                    try {
+                    obj = JSON.parse(data);
+                    if (obj.hasOwnProperty('data')){
                     message = obj['data'];
-                }
-                if (obj.hasOwnProperty('sender')){
+                    }
+                    if (obj.hasOwnProperty('sender')){
                     sender = obj['sender'];
-                }
-                if (obj.hasOwnProperty('cmd')){
+                    }
+                    if (obj.hasOwnProperty('cmd')){
                     cmd = obj['cmd'];
+                    }
+                    console.log(data);
+                    } catch (e) {};
+
+                    if (!message)
+                        message = data;
+
+                    var user = findCurrentSender(ws);
+                    if (user) {
+                        var pf = user_commander[cmd];
+                        if (pf) {
+                            var ret = pf[0](user, obj, message);
+                            // no broadcast if ret is zero
+                            if (ret == 0) return;
+                        } else if (cmd) {
+                            user.ws.send("unsupported cmd: " + cmd);
+                            return;
+                        }
+                    }
+
+                    if (user.state == WSC_STAT_TERMINATE) {
+                        user.ws.close(3000, 'admin stop');
+                        return;
+                    }
+
+                    /* broadcast to all users */
+                    user.msgcount++;
+                    var blob;
+                    if (user.hasOwnProperty('sender')) {
+                        blob = {sender: user.sender, data: message};
+                    } else if (sender) {
+                        blob = {sender: sender, data: message};
+                    } else {
+                        blob = {sender: 'guest', data: message};
+                    }
+
+                    blob.ts = UTCTimestamp();
+                    var msg = JSON.stringify(blob);
+                    cache_messages.push(msg);
+
+                    for (var i=0; i < clients.length ; i++) {
+                        var c = clients[i];
+                        c.ws.send(msg);
+                    }
+
+                });
+
+                ws.on('close', function () {
+                        var t = null;
+                        var mws = ws;
+                        for (var i=0; i < clients.length ; i++) {
+                        var c = clients[i];
+                        if (c.ws == mws) {
+                        t = c;
+                        clients.remove(t);
+                        break;
+                        }
+                        }
+                        if (t) {
+                        console.log('r%d: exit', t.id);
+                        }
+                        });
+
+                ws.send('Connected to chat server');
+                console.log("conn num: " + clients.length);
+
+                // while first login resend old messages in advance.
+                // FIXME: need to support get older message by paging
+                // FIXME:cache mechanism should have 
+                function restore_cache_messages() {
+                    var limit_restore_cache_count = 100;
+                    var cache_count = (cache_messages.length  > limit_restore_cache_count) ? limit_restore_cache_count : cache_messages.length; 
+                    console.log("test: " + cache_count.toString());
+                    for (var m=0; m <cache_count; m++) {
+                        ws.send(cache_messages[m]);
+                    }
                 }
-                console.log(data);
-            } catch (e) {};
-
-            if (!message)
-                message = data;
-
-            var user = findCurrentSender(ws);
-            if (user) {
-                var pf = user_commander[cmd];
-                if (pf) {
-                    var ret = pf[0](user, obj, message);
-                    // no broadcast if ret is zero
-                    if (ret == 0) return;
-                } else if (cmd) {
-                    user.ws.send("unsupported cmd: " + cmd);
-                    return;
-                }
-            }
-
-            if (user.state == WSC_STAT_TERMINATE) {
-                user.ws.close(3000, 'admin stop');
-                return;
-            }
-
-            /* broadcast to all users */
-            user.msgcount++;
-            for (var i=0; i < clients.length ; i++) {
-                var c = clients[i];
-                var blob;
-                if (user.hasOwnProperty('sender')) {
-                    blob = {sender: user.sender, data: message};
-                } else if (sender) {
-                    blob = {sender: sender, data: message};
-                } else {
-                    blob = {sender: 'guest', data: message};
-                }
-                var msg = JSON.stringify(blob);
-                c.ws.send(msg);
-                cache_messages.push(msg);
-            }
-
+                restore_cache_messages();
         });
 
-        ws.on('close', function () {
-            var t = null;
-            var mws = ws;
-            for (var i=0; i < clients.length ; i++) {
-                var c = clients[i];
-                if (c.ws == mws) {
-                    t = c;
-                    clients.remove(t);
-                    break;
+        var self = this;
+        self.send = function(data, flags) {
+            var sender = 'server'
+                try {
+                    for (var i=0; i < clients.length ; i++) {
+                        var c = clients[i];
+                        var blob = {sender: sender, data: message};
+                        c.ws.send(JSON.stringify(blob));
+                    }
+                } catch(e) {
+                    console.log("err");
                 }
-            }
-            if (t) {
-                console.log('r%d: exit', t.id);
-            }
-        });
+        };
 
-        ws.send('Connected to chat server');
-        console.log("conn num: " + clients.length);
-
-        // while first login resend old messages in advance.
-        // FIXME: need to support get older message by paging
-        // FIXME:cache mechanism should have 
-        function restore_cache_messages() {
-            var limit_restore_cache_count = 100;
-            var cache_count = (cache_messages.length  > limit_restore_cache_count) ? limit_restore_cache_count : cache_messages.length; 
-            console.log("test: " + cache_count.toString());
-            for (var m=0; m <cache_count; m++) {
-                ws.send(cache_messages[m]);
-            }
-        }
-        restore_cache_messages();
-    });
-
-    var self = this;
-    self.send = function(data, flags) {
-        var sender = 'server'
-            try {
-                for (var i=0; i < clients.length ; i++) {
-                    var c = clients[i];
-                    var blob = {sender: sender, data: message};
-                    c.ws.send(JSON.stringify(blob));
+        self.shutdown = false;
+        self.closeAll = function(data, flags) {
+            self.shutdown = true;
+            var sender = 'server'
+                try {
+                    for (var i=0; i < clients.length ; i++) {
+                        var c = clients[i];
+                        c.ws.close(3000, "chat v1 info closing...");
+                    }
+                } catch(e) {
+                    console.log("err");
                 }
-            } catch(e) {
-                console.log("err");
-            }
+
+            setTimeout(function () {
+                    console.log('server stopped.');
+                    process.exit(0);
+                    }, 1000);
+        };
+
+        console.log("jschat server listen on " +  server_port + "...");
     };
-
-    self.shutdown = false;
-    self.closeAll = function(data, flags) {
-        self.shutdown = true;
-        var sender = 'server'
-            try {
-                for (var i=0; i < clients.length ; i++) {
-                    var c = clients[i];
-                    c.ws.close(3000, "chat v1 info closing...");
-                }
-            } catch(e) {
-                console.log("err");
-            }
-
-        setTimeout(function () {
-                console.log('server stopped.');
-                process.exit(0);
-                }, 1000);
-    };
-
-    console.log("jschat server listen on " +  server_port + "...");
-};
 
 var chat_server = new ChatServer();
 
